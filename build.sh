d40c3ef7f1a5a3a3b9e0c86f1280d16b59abd5aa
Jardel Torres <74869343+jardimdanificado@users.noreply.github.com>
2024-03-18 02:06:58 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 pkg update -y
 pkg upgrade -y
 pkg install git build-essential clang libllvm libllvm-static llvmgold llvm-tools libpolly mlir cmake libedit luajit binutils -y
 git clone https://github.com/terralang/terra
 cd terra/build
 cmake -DCMAKE_INSTALL_PREFIX=$PWD/../install ..
-make install -j4 # tune this for how many cores you have
+make install -j3 # tune this for how many cores you have
