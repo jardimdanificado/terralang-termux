ded0cf9f75f64a6c3717d3d162df8af806582212
Jardel Torres <74869343+jardimdanificado@users.noreply.github.com>
2024-03-18 03:06:09 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ pkg install libffi libxml2 -y
 cp -r terra/install/bin/* ../../usr/bin/
 cp -r terra/install/include/* ../../usr/include/
 cp -r terra/install/lib/* ../../usr/lib/
-cp -r terra/install/shared/* ../../usr/shared/
-echo "terra installed."
+cp -r terra/install/share/* ../../usr/share/
+echo "terra installed on termux. o/"
